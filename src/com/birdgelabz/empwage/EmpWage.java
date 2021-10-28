4af7dd481208b8a63d19b435494b0a9af80409b2
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-28 16:43:11 +0530

    Added:Full time emp wage

--- src/com/birdgelabz/empwage/EmpWage.java
+++ src/com/birdgelabz/empwage/EmpWage.java
@@ -5,6 +5,7 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage");
 		EmpCheck();
+		FullTimeWage();
 
 	}
 
@@ -23,4 +24,26 @@ public class EmpWage {
 			System.out.println("Employee is Absent");
 	}
 
+	public static void FullTimeWage() {
+		// UC-2-Calculate daily full time employee wage
+
+		// Constants
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+
+		// Variables
+		int empHrs = 0;
+		int empWage = 0;
+
+		// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		System.out.println("Employee is FullTime");
+
+		// Formula
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage for full time is: " + empWage);
+	}
 }
