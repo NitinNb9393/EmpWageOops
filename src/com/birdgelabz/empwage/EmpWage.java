b4560c97e70421d512b52d65316682fe8d91e636
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-28 16:53:04 +0530

    Added:Solved using switch case

--- src/com/birdgelabz/empwage/EmpWage.java
+++ src/com/birdgelabz/empwage/EmpWage.java
@@ -7,6 +7,7 @@ public class EmpWage {
 		EmpCheck();
 		FullTimeWage();
 		PartTimeWage();
+		UsingSwitchCase();
 	}
 
 	public static void EmpCheck() {
@@ -77,5 +78,42 @@ public class EmpWage {
 		// Formula
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee Wage is: " + empWage);
+	}
+
+	public static void UsingSwitchCase() {
+		// UC-4-Solve employee wage using case statement
+
+		// Constants
+		final int IS_FULL_TIME = 1;
+		final int IS_PART_TIME = 2;
+		final int EMP_RATE_PER_HOUR = 20;
+
+		// Variables
+		int empHrs = 0;
+		int empWage = 0;
+
+		// Computation
+		int empCheck = (int) (Math.random() * 100) % 3;
+
+		switch (empCheck) {
+		case IS_FULL_TIME:
+			empHrs = 8;
+			System.out.println("Employee is present full time");
+			break;
+
+		case IS_PART_TIME:
+			empHrs = 4;
+			System.out.println("Employee is present part time");
+			break;
+
+		default:
+			empHrs = 0;
+			System.out.println("Employee is absent");
+			break;
+
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage is :" + empWage);
+
 	}
 }
