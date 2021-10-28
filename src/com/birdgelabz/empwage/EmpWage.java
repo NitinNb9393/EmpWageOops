9722cf015524484870c185fe1400a9755b1ba42d
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-28 16:48:33 +0530

    Added:Part time emp wage

--- src/com/birdgelabz/empwage/EmpWage.java
+++ src/com/birdgelabz/empwage/EmpWage.java
@@ -6,7 +6,7 @@ public class EmpWage {
 		System.out.println("Welcome to employee wage");
 		EmpCheck();
 		FullTimeWage();
-
+		PartTimeWage();
 	}
 
 	public static void EmpCheck() {
@@ -45,5 +45,37 @@ public class EmpWage {
 		// Formula
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee Wage for full time is: " + empWage);
+	}
+
+	public static void PartTimeWage() {
+		// UC-3-Part time employee wage calculation
+
+		// Constants
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+
+		// Variables
+		int empHrs = 0;
+		int empWage = 0;
+
+		// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+
+		if (empCheck == IS_PART_TIME) {
+			empHrs = 4;
+			System.out.println("Employee is PartTime");
+		}
+
+		else if (empCheck == IS_FULL_TIME) {
+			empHrs = 8;
+			System.out.println("Employee is FullTime");
+		} else {
+			System.out.println("Employee is Absent");
+		}
+
+		// Formula
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage is: " + empWage);
 	}
 }
